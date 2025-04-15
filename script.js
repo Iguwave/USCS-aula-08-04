1989054df0e8f98c0008670394417cefcc9192f2
Igor M silva <127322638+Iguwave@users.noreply.github.com>
2025-04-15 20:18:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function Soma()
+{   
+    var n = parseInt(prompt("Digite o primeiro número: "));
+    var n2 = parseInt(prompt("Digite o segundo número: "));
+    alert("Resultado: " + (n+n2))
+}
+
+function Subtracao()
+{
+    var n = parseInt(prompt("Digite o primeiro número: "));
+    var n2 = parseInt(prompt("Digite o segundo número: "));
+    alert("Resultado: " + (n-n2))
+}
+
+function Divisao()
+{
+    var n = parseInt(prompt("Digite o primeiro número: "));
+    var n2 = parseInt(prompt("Digite o segundo número: "));
+    alert("Resultado: " + (n/n2))
+}
+
+function Multiplicacao()
+{
+    var n = parseInt(prompt("Digite o primeiro número: "));
+    var n2 = parseInt(prompt("Digite o segundo número: "));
+    alert("Resultado: " + (n*n2))
+}
